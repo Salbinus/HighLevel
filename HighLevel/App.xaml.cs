93a6ac7aff897612c00ec645f68fe120afefe80b
aalbinus <alexander.albinus@payla.de>
2024-06-24 09:25:02 +0200

    project init

---
+++ HighLevel/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace HighLevel;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ HighLevel/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace HighLevel;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
